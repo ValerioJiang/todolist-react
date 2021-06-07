b2257bd47ed666d5df2788df62e3b5a22ff61534
Valerio Jiang <jiangvalerio1998@gmail.com>
2021-06-07 13:38:17 +0200

    Setup

---
+++ todolist/src/components/Header/HeaderComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class HeaderComponent extends Component {
+    render() {
+        return (
+            <div>
+                Ciao
+            </div>
+        )
+    }
+}
+
+export default HeaderComponent
